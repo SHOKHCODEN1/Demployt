dd75a9679f2601474064164eecae4ad113428005
SHOKHCODERN1 <zokirovshohjahon12@gmail.com>
2025-12-15 15:40:16 +0300

    first commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3!#9u0nv=2oxicojd-c^h--1&*#7xo_$vm-=)&!^zhgl%#$3r8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
